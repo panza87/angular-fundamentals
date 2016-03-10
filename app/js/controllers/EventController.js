5ab4021bf74eb692dd59e5f135fb98082c502518
Simon Panzarino <sdpanzarino@hotmail.com>
2016-03-10 13:25:45 +1100

    First commit, files saved

---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,57 @@
+'use strict';
+
+eventsApp.controller('EventController', function EventController($scope) {
+	
+	$scope.snippet = '<span style="color:red">Hi there</span>';
+	$scope.boolValue = true;
+	$scope.mystyle = {
+		color: 'red'
+	};
+	$scope.myclass = "blue";
+	$scope.buttonDisabled = true;
+	$scope.event = {
+		name: 'Angular Boot Camp',
+		date: '1/1/2013',
+		time: '10:30 am',
+		location: {
+			address: 'Google Headquarters',
+			city: 'Mountain View',
+			province: 'CA'
+		},
+		imageUrl: 'img/angularjs-logo.png',
+		sessions: [
+			{
+				name: 'Directive Maserclass',
+				creatorName: 'Bob Smith',
+				duration: '1 hr',
+				level: 'Advanced',
+				abstract: 'In this session you will learn the ins and outs.....',
+				upVoteCount: 0
+			},
+			{
+				name: 'Scopes for fun and profit',
+				creatorName: 'John Doe',
+				duration: '30 mins',
+				level: 'Introductory',
+				abstract: 'This session will take a caloser look at scopes......',
+				upVoteCount: 0
+			},
+			{
+				name: 'Well Behaved Controllers',
+				creatorName: 'Jane Doe',
+				duration: '2 hours',
+				level: 'Intermediate',
+				abstract: 'Controllers are the beginning of everything Angular does....',
+				upVoteCount: 0
+			}
+		]
+	}
+	
+	$scope.upVoteSession = function(session) {
+		session.upVoteCount++;
+	};
+	
+	$scope.downVoteSession = function(session) {
+		session.upVoteCount--;
+	}
+})
