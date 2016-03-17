ba91dbe9ee91e92e4a29e1f53add738360177988
Simon Panzarino <sdpanzarino@hotmail.com>
2016-03-17 17:51:07 +1100

    Replace http with resource for resftul API

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,13 +1,14 @@
-eventsApp.factory('eventData', function ($http, $log) {
+eventsApp.factory('eventData', function ($resource) {
+	var resource = $resource('/data/event/:id', {
+		id: '@id'
+	});
 	return {
-		getEvent: function (successcb) {
-			$http({method: 'GET', url: '/data/event/1'})
-				.success(function (data, status, headers, config) {
-					successcb(data);
-				})
-				.error(function (data, status, headers, config) {
-					$log.warn(data, status, headers(), config);
-				});
+		getEvent: function() {
+			return resource.get({id:1});
+		},
+		save: function(event) {
+			event.id = 987;
+			return resource.save(event);
 		}
 	};
 });
