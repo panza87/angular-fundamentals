27a9e37530875f71c70fc0d00c869304175dc697
Simon Panzarino <sdpanzarino@hotmail.com>
2016-03-18 17:11:04 +1100

    Fix events page, resource variable was wrong

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,5 +1,5 @@
 eventsApp.factory('eventData', function($resource) {
-    var resource = $resource('/data/event/:id', {id:'@id'}, {"getAll": {method: "GET", isArray: true, params: {something: "foo"}}});
+    var resource = $resource('/data/event/:id', {id:'@id'}, {"getAll": {method: "GET", isArray: true}});
     return {
         getEvent: function() {
             return resource.get({id:1});
