e5983299ce79c3ab7f0c894e1b0aaa9efb34c397
Simon Panzarino <sdpanzarino@hotmail.com>
2016-03-22 19:29:07 +1100

    Add custom directive

--- app/js/app.js
+++ app/js/app.js
@@ -22,6 +22,11 @@ var eventsApp = angular.module('eventsApp', ['ngResource', 'ngRoute'])
                 templateUrl: 'templates/EventDetails.html',
                 controller: 'EventController',
             });
+		$routeProvider.when('/sampleDirective',
+            {
+                templateUrl: 'templates/SampleDirective.html',
+                controller: 'SampleDirectiveController',
+            });
         $routeProvider.otherwise({redirectTo: '/events'});
 		$locationProvider.html5Mode(true);
 
---
+++ app/js/controllers/SampleDirectiveController.js
@@ -0,0 +1,5 @@
+'use strict';
+
+eventsApp.controller('SampleDirectiveController', function() {
+	
+});
